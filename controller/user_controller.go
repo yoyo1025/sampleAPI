c70e07aba515b28eaafc5ee5f49364935ca3d51c
yoyo <hsjejxys@gmail.com>
2024-04-07 20:03:45 +0900

    LogInメソッドを実装

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -2,8 +2,10 @@ package controller
 
 import (
 	"net/http"
+	"os"
 	"sampleAPI/model"
 	"sampleAPI/usecase"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -33,4 +35,26 @@ func (uc *userController) SignUp(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
 	return c.JSON(http.StatusCreated, userRes)
+}
+
+func (uc *userController) LogIn(c echo.Context) error {
+	user := model.User{}
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	tokenString, err := uc.uu.Login(user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	cookie := new(http.Cookie)
+	cookie.Name = "token"
+	cookie.Value = tokenString
+	cookie.Expires = time.Now().Add(24 * time.Hour)
+	cookie.Path = "/"
+	cookie.Domain = os.Getenv("API_DOMAIN")
+	// cookie.Secure = true
+	cookie.HttpOnly = true
+	cookie.SameSite = http.SameSiteNoneMode
+	c.SetCookie(cookie)
+	return c.NoContent(http.StatusOK)
 }
