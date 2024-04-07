2b418562626d0538532888215ee8f4d61ac3818f
yoyo <hsjejxys@gmail.com>
2024-04-07 18:57:33 +0900

    userUsecaseモデルを定義

--- usecase/user_usecase.go
+++ usecase/user_usecase.go
@@ -1,8 +1,17 @@
 package usecase
 
-import "sampleAPI/model"
+import (
+	"sampleAPI/model"
+	"sampleAPI/repository"
+)
 
 type IUserUsecase interface {
 	SignUp(user model.User) (model.UserResponse, error)
 	Login(user model.User) (string, error)
 }
+
+// usecaseはrepositoryインターフェースだけに依存する
+type userUsecase struct {
+	ur repository.IUserRepository
+}
+
