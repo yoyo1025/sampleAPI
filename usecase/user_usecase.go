0da3f403f91e1be239761354af88a3b9a1733291
yoyo <hsjejxys@gmail.com>
2024-04-07 19:22:44 +0900

    Loginメソッドの実装

--- usecase/user_usecase.go
+++ usecase/user_usecase.go
@@ -1,9 +1,12 @@
 package usecase
 
 import (
+	"os"
 	"sampleAPI/model"
 	"sampleAPI/repository"
+	"time"
 
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -36,4 +39,21 @@ func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
 		Email: newUser.Email,
 	}
 	return resUser, nil
+}
+
+func (uu *userUsecase) Login(user model.User) (string, error) {
+	storedUser := model.User{}
+	if err := uu.ur.GetUserByEmail(&storedUser, user.Email); err != nil {
+		return "", err
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(storedUser.Password), []byte(user.Password))
+	if err != nil {
+		return "", err
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": storedUser.ID,
+		"exp": time.Now().Add(time.Hour * 12).Unix(),
+	})
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+	return tokenString, nil
 }
