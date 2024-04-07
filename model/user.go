a5f77531a4fb4b910f54511a48473965dc73f847
yoyo <hsjejxys@gmail.com>
2024-04-07 17:40:46 +0900

    gorm を導入

--- model/user.go
+++ model/user.go
@@ -3,14 +3,14 @@ package model
 import "time"
 
 type User struct {
-	ID uint `json:"id"`
-	Email string `json:"email"`
+	ID uint `json:"id" gorm:"primaryKey"`
+	Email string `json:"email" gorm:"unique"` 
 	Password string `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type UserResponse struct {
-	ID uint `json:"id"`
-	Email string `json:"email"`
+	ID uint `json:"id" gorm:"primaryKey"`
+	Email string `json:"email" gorm:"unique"`
 }
