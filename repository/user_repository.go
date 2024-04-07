d004370598c5d5346852845f6f9af9a6bc2649cc
yoyo <hsjejxys@gmail.com>
2024-04-07 19:26:13 +0900

    userRepositoryモデルを追加

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -1,9 +1,17 @@
 package repository
 
-import "sampleAPI/model"
+import (
+	"sampleAPI/model"
+
+	"gorm.io/gorm"
+)
 
 // UserRepositoryのインターフェースを定義
 type IUserRepository interface {
 	GetUserByEmail(user *model.User, email string) error
 	CreateUser(user *model.User) error
+}
+
+type userResitory struct {
+	db *gorm.DB
 }
