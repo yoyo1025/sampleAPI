a4b74c55b19dd384b47db45003a26b4f9f2a4f90
yoyo <hsjejxys@gmail.com>
2024-04-07 19:44:43 +0900

    userControllerモデルを定義

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -1,9 +1,17 @@
 package controller
 
-import "github.com/labstack/echo/v4"
+import (
+	"sampleAPI/usecase"
+
+	"github.com/labstack/echo/v4"
+)
 
 type IUserController interface {
 	SignUp(c echo.Context) error
 	LogIn(c echo.Context) error
 	LogOut(c echo.Context) error
+}
+
+type userController struct {
+	uu usecase.IUserUsecase
 }
