dd1e169f304c79cd9c2d95c9a99956ad1f48d9a2
yoyo <hsjejxys@gmail.com>
2024-04-07 18:02:08 +0900

    taskモデルの作成

---
+++ model/task.go
@@ -0,0 +1,12 @@
+package model
+
+import "time"
+
+type Task struct {
+	ID        uint   `json:"id" gorm:"primaryKey"`
+	Title     string `json:"title" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	User User `json:"user" gorm:"foreignKey:UserId; constraint:OnDelete:CASCADE"` // userを削除するとuserに紐づいたタスクがすべて削除される
+	UserId uint `json:"user_id" gorm:"not null"`
+}
