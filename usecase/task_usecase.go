1ba9b6a5925a3d3577cbde961e21b2fa3958e2f8
yoyo <hsjejxys@gmail.com>
2024-04-09 00:05:04 +0900

    CreateTaskメソッドを実装

--- usecase/task_usecase.go
+++ usecase/task_usecase.go
@@ -54,3 +54,16 @@ func (tu *taskUsecase) GetTaskById(userId uint, taskId uint) (model.TaskResponse
 	}
 	return resTask, nil
 }
+
+func (tu *taskUsecase) CreateTask(task model.Task) (model.TaskResponse, error) {
+	if err := tu.tr.CreateTask(&task); err != nil {
+		return model.TaskResponse{}, err
+	}
+	resTask := model.TaskResponse{
+		ID:        task.ID,
+		Title:     task.Title,
+		CreatedAt: task.CreatedAt,
+		UpdatedAt: task.UpdatedAt,
+	}
+	return resTask, nil
+}
