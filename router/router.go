19a4444ac30dab33f4212f78bed08416140ccb2b
yoyo <hsjejxys@gmail.com>
2024-04-14 00:59:16 +0900

    SameSiteNoneModeに変更

--- router/router.go
+++ router/router.go
@@ -23,8 +23,8 @@ func NewRouter(uc controller.IUserController, tc controller.ITaskController) *ec
 		CookiePath: "/",
 		CookieDomain: os.Getenv("API_DOMAIN"),
 		CookieHTTPOnly: true,
-		// CookieSameSite: http.SameSiteNoneMode,
-		CookieSameSite: http.SameSiteDefaultMode, // PostMan用
+		CookieSameSite: http.SameSiteNoneMode,
+		// CookieSameSite: http.SameSiteDefaultMode, // PostMan用
 		// CookieMaxAge: 60,
 	}))
 	e.POST("/signup", uc.SignUp)
