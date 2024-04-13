8c4830863654c15281569be0fc42e1c787ac6f56
yoyo <hsjejxys@gmail.com>
2024-04-13 17:51:28 +0900

    NewTaskValidatorコンストラクタを作成

--- validator/task_validator.go
+++ validator/task_validator.go
@@ -5,3 +5,11 @@ import "sampleAPI/model"
 type ITaskValidator interface {
 	TaskValidate(task model.Task) error
 }
+
+type taskValidator struct{}
+
+// taskValidator構造体を作成するためのコンストラクタ
+func NewTaskValidator() ITaskValidator {
+	return &taskValidator{}
+}
+
