4043d481918efb18b77991af93fe9a036bc993f2
yoyo <hsjejxys@gmail.com>
2024-04-13 14:29:33 +0900

    GetAllTasksメソッドを作成

--- controller/task_controller.go
+++ controller/task_controller.go
@@ -1,8 +1,10 @@
 package controller
 
 import (
+	"net/http"
 	"sampleAPI/usecase"
 
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
@@ -20,4 +22,16 @@ type taskController struct {
 
 func NewTaskController(tu usecase.ITaskUsecase) ITaskController {
 	return &taskController{tu}	
+}
+
+func (tc *taskController) GetAllTasks(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token) // デコード
+	claims := user.Claims.(jwt.MapClaims)
+	userId := claims["user_id"]
+
+	tasksRes, err := tc.tu.GetAllTasks(uint(userId.(float64)))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusOK, tasksRes)
 }
