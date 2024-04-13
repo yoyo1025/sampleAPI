7271e90778a4710ba269d3b3cfd7405b4bfdb067
yoyo <hsjejxys@gmail.com>
2024-04-13 14:18:44 +0900

    TaskControllerインターフェースを定義

---
+++ controller/task_controller.go
@@ -0,0 +1,11 @@
+package controller
+
+import "github.com/labstack/echo/v4"
+
+type ITaskController interface {
+	GetAllTasks(c echo.Context) error
+	GetTaskById(c echo.Context) error
+	CreateTask(c echo.Context) error
+	UpdateTask(c echo.Context) error
+	DeleteTask(c echo.Context) error
+}
