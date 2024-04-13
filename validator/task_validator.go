bffd218dc06f9294120074c104135d6c086f12e9
yoyo <hsjejxys@gmail.com>
2024-04-13 17:56:14 +0900

    TaskValidateメソッドを実装

--- validator/task_validator.go
+++ validator/task_validator.go
@@ -1,6 +1,10 @@
 package validator
 
-import "sampleAPI/model"
+import (
+	"sampleAPI/model"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
 
 type ITaskValidator interface {
 	TaskValidate(task model.Task) error
@@ -13,3 +17,11 @@ func NewTaskValidator() ITaskValidator {
 	return &taskValidator{}
 }
 
+func (tv *taskValidator) TaskValidate(task model.Task) error {
+	return validation.ValidateStruct(&task,
+		validation.Field(
+			&task.Title,
+			validation.Required.Error("title is required"),
+			validation.RuneLength(1, 10).Error("limited max 10 char"),
+		))
+}
